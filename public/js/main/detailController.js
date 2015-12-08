74f21bf4b799ffadaf3ff1ef849800d7ce46e839
liuxiangyu <xiangyu.liu@dianping.com>
2015-12-08 16:06:22 +0800

    add tab bar

--- public/js/main/detailController.js
+++ public/js/main/detailController.js
@@ -1,5 +1,9 @@
 'use strict';
 
 app.controller('detailController', function ($scope, $rootScope) {
+  $("#centeredmenu ul li a").click(function(){
+    $("#centeredmenu ul li a").removeClass("active");
+    $(this).toggleClass("active");
+  });
 });
 
